a24a0a2d34e5e6a032a9f063bc441bb1e2ef618d
overdeliver <ioverdeliver@gmail.com>
2017-07-31 15:40:08 -0400

    Test commit.

--- core/src/com/sampler/InputPollingSample.kt
+++ core/src/com/sampler/InputPollingSample.kt
@@ -19,7 +19,7 @@ class InputPollingSample : ApplicationListener {
     internal lateinit var font: BitmapFont
 
     override fun render() {
-        // clear screen.
+        // clear screen. Merging.
         Gdx.gl.glClearColor(0F, 0F, 0F, 1.0F)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
