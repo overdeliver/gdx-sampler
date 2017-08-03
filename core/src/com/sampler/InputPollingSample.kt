740c3bc8a09c5d3c412915b205cad6de7f479e60
overdeliver <ioverdeliver@gmail.com>
2017-08-03 18:23:58 -0400

    Added InputPollingSample.

--- core/src/com/sampler/InputPollingSample.kt
+++ core/src/com/sampler/InputPollingSample.kt
@@ -3,6 +3,7 @@ package com.sampler
 import com.badlogic.gdx.Application
 import com.badlogic.gdx.ApplicationListener
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.g2d.BitmapFont
@@ -19,14 +20,64 @@ class InputPollingSample : ApplicationListener {
     internal lateinit var font: BitmapFont
 
     override fun render() {
-        // clear screen. Merging.
+        // clear screen.
         Gdx.gl.glClearColor(0F, 0F, 0F, 1.0F)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
         batch.projectionMatrix = camera.combined
         batch.begin()
 
+        draw()
+
         batch.end()
+    }
+
+    fun draw() {
+        // mouse / touch x/y
+        val mouseX = Gdx.input.x
+        val mouseY = Gdx.input.y
+
+        // buttons
+        val leftPressed = Gdx.input.isButtonPressed(Input.Buttons.LEFT)
+        val rightPressed = Gdx.input.isButtonPressed(Input.Buttons.RIGHT)
+
+        font.draw(batch,
+                "Mouse/Touch : x= $mouseX y= $mouseY",
+                20F,
+                720 - 20F)
+
+        font.draw(batch,
+                if(leftPressed) {
+                    "Left button pressed"
+                } else "Left button not pressed",
+                20F,
+                720 - 50F)
+
+        font.draw(batch,
+                if(rightPressed) {
+                    "Right button pressed"
+                } else
+                    "Right button not pressed",
+                20F,
+                720 - 80F)
+
+        val wPressed = Gdx.input.isKeyPressed(Input.Keys.W)
+        val sPressed = Gdx.input.isKeyPressed(Input.Keys.S)
+
+        font.draw(batch,
+                if(wPressed) {
+                    "W is pressed"
+                } else "W is not pressed",
+                20F,
+                720 - 110F)
+
+        font.draw(batch,
+                if(sPressed) {
+                    "S is pressed pressed"
+                } else
+                    "S is not pressed",
+                20F,
+                720 - 140F)
     }
 
     override fun pause() {
@@ -38,7 +89,7 @@ class InputPollingSample : ApplicationListener {
     }
 
     override fun resize(width: Int, height: Int) {
-        viewport.update(width, height)
+        viewport.update(width, height, true)
     }
 
     override fun create() {
@@ -47,13 +98,15 @@ class InputPollingSample : ApplicationListener {
         camera = OrthographicCamera()
         viewport = FitViewport(1080F, 720F, camera)
         batch = SpriteBatch()
-        font = BitmapFont(Gdx.files.internal("fonts/oswald-32.fnt"))
+        font = BitmapFont(Gdx.files.internal("/home/io/Projects/gdx-sampler/core/assets/fonts/oswald-32.fnt"))
     }
 
     override fun dispose() {
         batch.dispose()
         font.dispose()
     }
+
+
 
     companion object {
         private val log = Logger(ApplicationListenerSample::class.java.name, Logger.DEBUG)
